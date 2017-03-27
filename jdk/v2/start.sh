095eabec17407df62069afe7a8839c61d095f5b5
chenyongbing <cyb880326>
2017-03-27 11:12:09 +0800

    add v1 and v2

---
+++ jdk/v2/start.sh
@@ -0,0 +1,3 @@
+set -e
+JAVA_OPTS="-ms128m -mx512m -Xmn256m -Djava.awt.headless=true -XX:MaxPermSize=128m -Duser.timezone=Asia/Shanghai"
+java -Dproject.build.version=$KS_PROJECT_VERSION $JAVA_OPTS  -Dloader.path=/data/$KS_PROJECT_NAME/resources -jar /data/$KS_PROJECT_NAME/libs/*.jar > /dev/null  2>&1
