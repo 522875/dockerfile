76f860f2f1f43c8f1877fc0f2265edda8d7beb8e
chenyongbing <cyb880326>
2017-04-28 17:04:50 +0800

    add env

--- jdk/v2/start.sh
+++ jdk/v2/start.sh
@@ -1,3 +1,2 @@
 set -e
-JAVA_OPTS="-ms128m -mx1024m  -Djava.awt.headless=true -XX:MaxPermSize=128m -Duser.timezone=Asia/Shanghai"
-java -Dproject.build.version=$KS_PROJECT_VERSION -Dserver.port=$KS_PROJECT_PORT $JAVA_OPTS  -Dloader.path=/data/$KS_PROJECT_NAME/resources -jar /data/$KS_PROJECT_NAME/libs/*.jar
+java -Dproject.build.version=$KS_PROJECT_VERSION -Dserver.port=$KS_PROJECT_PORT  $JAVA_OPTS -ms128m -mx1024m  -Djava.awt.headless=true -XX:MaxPermSize=128m -Duser.timezone=Asia/Shanghai  -Dloader.path=/data/$KS_PROJECT_NAME/resources -jar /data/$KS_PROJECT_NAME/libs/*.jar
